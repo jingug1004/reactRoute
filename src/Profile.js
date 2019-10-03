50b4812b45f6b20ec8ec1da506f08db8b802466a
jingug1004 <jingug1004@gmail.com>
2019-10-03 15:14:28 +0900

    191003(목) 리액트에 라우트 설정_03

---
+++ src/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const data = {
+    velopert: {
+        name: '김민준',
+        description: '리액트를 좋아'
+    },
+    gildong: {
+        name: '홍길동',
+        description: '고전소설 주인공'
+    }
+
+}
+
+const Profile = ({match}) => {
+    const {username} = match.params;
+    const profile = data[username];
+    if (!profile) {
+        return <div>존재하지 않는...</div>;
+    }
+
+
+    return (
+        <div>
+            <h3>{username}({profile.name})</h3>
+            <p>{profile.description}</p>
+        </div>
+    );
+};
+
+export default Profile;
