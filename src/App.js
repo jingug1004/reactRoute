dad17a10d878f43de42afdebe6dd64ea185c12a3
jingug1004 <jingug1004@gmail.com>
2019-10-03 14:46:33 +0900

    191003(목) 리액트에 라우트 설정_02

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
             <hr/>
 
             <Route path={"/"} component={Home} exact={true}/>
-            <Route path={"/about"} component={About}/>
+            <Route path={['/about', '/info']} component={About}/>
 
         </div>
     );
